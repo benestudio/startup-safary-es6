aaf20b0737246a3562ddadccdf47b0c969619673
tudvari <tibor.udvari@nng.com>
2017-04-20 10:13:11 +0200

    destructing, template strings

---
+++ destructing.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var a, b, rest
+[a, b, rest] = [10, 20, 30, 40, 50]
+
+console.log(a)
+console.log(b)
+console.log(rest)
---
+++ stringliterals.js
@@ -0,0 +1,6 @@
+'use strict'
+
+var a = 5
+var b = 10
+
+console.log(`Fifteen is ${a + b} and not ${2 * a + b}.`)
