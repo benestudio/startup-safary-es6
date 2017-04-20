d2597b486c5a1aba99fd99faba6a4b5b416d513d
tudvari <tibor.udvari@nng.com>
2017-04-20 09:37:15 +0200

    initial commit + generator

---
+++ generator.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var fibonacci = {
+	[Symbol.iterator]: function * () {
+		var pre = 0, cur = 1
+		for (;;) {
+			var temp = pre
+			pre = cur
+			cur += temp
+			yield cur
+		}
+	}
+}
+
+for (var n of fibonacci) {
+	if (n > 1000)
+		break
+	console.log(n)
+}
